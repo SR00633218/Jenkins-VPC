1f371427232bceceed40d30837974c28534121d6
SR00633218 <60056161+SR00633218@users.noreply.github.com>
2021-08-15 15:09:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,138 @@
+resource "aws_vpc" "ntiervpc" {
+    cidr_block = var.ntier_cidr
+    tags = {
+        "Name" = "ntier"
+             
+    }
+      
+  
+}
+resource "aws_subnet" "subnets" {
+    count = length(var.ntier_subnets_azs)
+    cidr_block = cidrsubnet(var.ntier_cidr, 8, count.index)
+    availability_zone = var.ntier_subnets_azs[count.index]
+    tags = {
+      "Name" = var.ntier_subnets_tags[count.index]
+    }
+    vpc_id = aws_vpc.ntiervpc.id
+
+
+  
+}
+resource "aws_internet_gateway" "ntier-igw" {
+    vpc_id = aws_vpc.ntiervpc.id
+    tags = {
+      "Name" = "ntier-igw"
+    }
+     depends_on = [
+    aws_vpc.ntiervpc
+  ]
+  
+}
+resource "aws_route_table" "publicrt" {
+  vpc_id = aws_vpc.ntiervpc.id
+  route = [ ]
+
+  tags = {
+    "Name" = "ntier-publicrt"
+  }
+  depends_on = [
+    aws_vpc.ntiervpc,
+    aws_subnet.subnets  
+  ]
+}
+
+resource "aws_route" "publicroute" {
+  route_table_id = aws_route_table.publicrt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.ntier-igw.id
+}
+
+resource "aws_route_table_association" "publicrtassociations" {
+  count = length(var.web_subnet_indexes)
+  subnet_id = aws_subnet.subnets[var.web_subnet_indexes[count.index]].id
+  route_table_id = aws_route_table.publicrt.id
+}
+resource "aws_security_group" "websg" {
+  name = "openhttp"
+  description = "Open http and ssh"
+  vpc_id = aws_vpc.ntiervpc.id
+
+  tags = {
+    "Name" = "Openhttp"
+  }
+
+}
+
+resource "aws_security_group_rule" "websghttp" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.websg.id
+
+}
+
+resource "aws_security_group_rule" "websgssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.websg.id
+
+}
+resource "aws_instance" "webserver1" {
+  ami = "ami-04db49c0fb2215364" 
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [aws_security_group.websg.id]
+  subnet_id = aws_subnet.subnets[0].id
+  key_name = "Master"
+  depends_on = [
+    aws_vpc.ntiervpc,
+    aws_route_table.publicrt
+
+  ]
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("./Master.pem")
+    host = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo hello",
+      "set",
+      "pwd",
+      "sudo apt update",
+      "sudo apt install apache2 -y"
+    ]
+
+  }
+     
+}
+resource "aws_route_table" "ntierprivatert" {
+  vpc_id = aws_vpc.ntiervpc.id
+  route = [ ]
+
+  tags = {
+    "Name" = "ntier-privatert"
+  }
+
+}
+
+resource "aws_route_table_association" "privatertassociations" {
+  count = length(var.other_subnet_indexes)
+  subnet_id = aws_subnet.subnets[var.other_subnet_indexes[count.index]].id
+  route_table_id = aws_route_table.ntierprivatert.id
+
+  depends_on = [
+    aws_subnet.subnets,
+    aws_route_table.ntierprivatert
+  ]
+
+
+}
---
+++ values.tfvars
@@ -0,0 +1,5 @@
+ntier_cidr = "10.1.0.0/16"
+ntier_subnets_tags = ["ntier-web1", "ntier-app1", "ntier-db1", "ntier-web2", "ntier-app2", "ntier-db2"]
+ntier_subnets_azs = ["ap-south-1a", "ap-south-1a", "ap-south-1a", "ap-south-1b", "ap-south-1b", "ap-south-1b"]
+web_subnet_indexes = [0,3] 
+other_subnet_indexes = [ 1,2,4,5] 
